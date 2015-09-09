39c6deb99999b62d380352991d4605cf67dff0d4
mrkkrp <markkarpov@opmbx.org>
2015-09-09 22:15:58 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Modalka
+
+[![License GPL 3](https://img.shields.io/badge/license-GPL_3-green.svg)](http://www.gnu.org/licenses/gpl-3.0.txt)
+
+TODO Write description and the same time clarifying the conception for
+myself.
+
+## Installation
+
+If you would like to install the package manually, download or clone it and
+put on Emacs' `load-path`, then you can require it in your init file like
+this:
+
+```emacs-lisp
+(require 'modalka)
+```
+
+## Usage
+
+Coming soon…
+
+## Customization
+
+Coming soon…
+
+## License
+
+Copyright © 2015 Mark Karpov
+
+Distributed under GNU GPL, version 3.
---
+++ modalka.el
@@ -0,0 +1,36 @@
+;;; modalka.el --- Easily introduce native modal editing of your own design -*- lexical-binding: t; -*-
+;;
+;; Copyright © 2015 Mark Karpov <markkarpov@openmailbox.org>
+;;
+;; Author: Mark Karpov <markkarpov@openmailbox.org>
+;; URL: https://github.com/mrkkrp/modalka
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.4"))
+;; Keywords: modal editing
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation, either version 3 of the License, or (at your
+;; option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+;; Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; TODO Add description.
+
+;;; Code:
+
+(require 'cl-lib)
+
+(provide 'modalka)
+
+;;; modalka.el ends here
