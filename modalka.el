5a1e914bb76dfde539df26f3108793bba98bd4f9
mrkkrp <markkarpov@opmbx.org>
2016-01-22 18:33:00 +0600

    Version bump

--- modalka.el
+++ modalka.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Mark Karpov <markkarpov@openmailbox.org>
 ;; URL: https://github.com/mrkkrp/modalka
-;; Version: 0.1.0
+;; Version: 0.1.2
 ;; Package-Requires: ((emacs "24.4"))
 ;; Keywords: modal editing
 ;;
