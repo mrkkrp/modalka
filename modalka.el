c7f4d4ed554d070f03787d657990ffec146740c0
mrkkrp <markkarpov@opmbx.org>
2016-05-27 22:07:46 +0700

    Satisfy Emacs' love for two spaces after a period

--- modalka.el
+++ modalka.el
@@ -25,9 +25,9 @@
 
 ;;; Commentary:
 
-;; This is a building kit to help switch to modal editing in Emacs. Main
+;; This is a building kit to help switch to modal editing in Emacs.  Main
 ;; goal of the package is making modal editing in Emacs as natural and
-;; native as possible. There is no hack, no corner cases, no emulation —
+;; native as possible.  There is no hack, no corner cases, no emulation —
 ;; just start edit modally the way you want.
 ;;
 ;; More information is available in the README.md file.
