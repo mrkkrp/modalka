e69ec8fa01e86cb789f7f2b27b6d5a47e1ca3069
mrkkrp <markkarpov92@gmail.com>
2018-01-01 13:13:19 +0700

    Include 2018 into the copyright years range

--- modalka.el
+++ modalka.el
@@ -1,6 +1,6 @@
 ;;; modalka.el --- Easily introduce native modal editing of your own design -*- lexical-binding: t; -*-
 ;;
-;; Copyright © 2015–2017 Mark Karpov <markkarpov92@gmail.com>
+;; Copyright © 2015–2018 Mark Karpov <markkarpov92@gmail.com>
 ;;
 ;; Author: Mark Karpov <markkarpov92@gmail.com>
 ;; URL: https://github.com/mrkkrp/modalka
