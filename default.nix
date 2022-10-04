07d81719713185db0d90d118fbd209e6aec41189
Mark Karpov <markkarpov92@gmail.com>
2022-10-04 13:50:45 +0200

    Update the CI setup, fail on warnings

--- default.nix
+++ default.nix
@@ -8,10 +8,12 @@ pkgs.stdenv.mkDerivation {
   name = "modalka";
   src = source;
   buildInputs = [
-    (pkgs.emacs26WithPackages (epkgs: []))
+    (pkgs.emacs28WithPackages (epkgs: []))
   ];
   buildPhase = ''
-    emacs -L . --batch -f batch-byte-compile *.el
+    emacs -L . --batch -f batch-byte-compile *.el 2> stderr.txt
+    cat stderr.txt
+    ! grep -q ': Warning:' stderr.txt
   '';
   installPhase = ''
     LISPDIR=$out/share/emacs/site-lisp
